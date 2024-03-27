d455cc69d0bc65c59574258d7e3a641b0bcdc6b8
Faisal Ahmad Gifari <faisal.ahmadgifari21@mhs.uinjkt.ac.id>
2024-03-27 19:57:19 +0700

    pertama

---
+++ script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const submitForm = document.getElementById('inputBook');
+  submitForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addBook();
+  });
+});
+
+function addBook() {
+  const bookTitle = document.getElementById('inputTitle').value;
+  const bookAuthor = document.getElementById('inputAuthor').value;
+  const bookYear = document.getElementById('inputYear').value;
+
+  // const timestamp = document.getElementById('date').value;
+ 
+  const generatedID = generateId();
+  const bookObject = generateBookObject(generatedID, bookTitle, bookAuthor, bookYear);
+  //const bookObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+  books.push(bookObject);
+ 
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+
+function makeBook(dummyAddBook) {
+
+  // Book Info
+  const bookTitle = document.createElement('h3');
+  bookTitle.innerText = dummyAddBook.title;
+
+  const bookAuthor = document.createElement('p');
+  bookAuthor.innerText = dummyAddBook.author;
+
+  const bookYear = document.createElement('p');
+  bookYear.innerText = dummyAddBook.year;
+
+  // Action Button
+  const doneButton = document.createElement('button').classList.add('green');
+  doneButton.innerText = 'Done Read';
+  const removeButton = document.createElement('button').classList.add('red');
+  removeButton.innerText = 'Remove Book';
+
+  // Action Container
+  const actionContainer = document.createElement('div');
+  actionContainer.classList.add('action');
+  actionContainer.append(doneButton, removeButton);
+
+
+  // Item Book Container 
+  const itemBookContainer = document.createElement('article');
+  itemBookContainer.classList.add('item_book');
+  itemBookContainer.append(bookTitle, bookAuthor, bookYear, actionContainer);
+  itemBookContainer.setAttribute('id', `book-${dummyAddBook.id}`);
+
+  return itemBookContainer;
+}
+
+
