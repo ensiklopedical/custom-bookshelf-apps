59670746a5e7b06a0032e25c9e69d87aa9bf53bc
Faisal Ahmad Gifari <faisal.ahmadgifari21@mhs.uinjkt.ac.id>
2024-03-29 09:17:23 +0700

    button undo dan done sudah ada terpisah

--- script.js
+++ script.js
@@ -32,6 +32,18 @@ document.addEventListener(RENDER_EVENT, function () {
   console.log('RENDER EVENT: aman');
 });
 
+const undo = document.getElementsByClassName('yellow');
+undo.addEventListener('click', function () {
+  bookObject.check = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+});
+
+const undoButton = document.getElementsByClassName('yellow');
+doneButton.addEventListener('click', function () {
+  bookObject.check = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+});
+
 
 function generateId() {
   // console.log('generateID: aman');
@@ -55,7 +67,6 @@ function addBook() {
   const bookYear = document.getElementById('inputYear').value;
   const bookCheck = document.getElementById('inputIsComplete').checked;
 
-  
   const generatedID = generateId();
   const bookObject = generateBookObject(generatedID, bookTitle, bookAuthor, bookYear, bookCheck);
   books.push(bookObject);
@@ -79,41 +90,76 @@ function makeBook(bookObject) {
   const bookYear = document.createElement('p');
   bookYear.innerText = bookObject.year;
 
+  /*
   // Action Button
   const removeButton = document.createElement('button');
   removeButton.classList.add('red');
   removeButton.innerText = "Remove Book";
 
+  const undoButton = document.createElement('button');
+  undoButton.classList.add('yellow');
+  undoButton.innerText = "Undo Read";
+
+  const doneButton = document.createElement('button');
+  doneButton.classList.add('green');
+  doneButton.innerText = "Done Read";
+
+  // Action Container
+  const actionContainer = document.createElement('div');
+  actionContainer.classList.add('action');
+  actionContainer.append(undoButton, doneButton, removeButton);
+  */
+
+  const actionContainer = document.createElement('div');
+  actionContainer.classList.add('action');
+
+  
   if (bookObject.check){
-    // =============Selesai Dibaca================
+    // =============Undo Button================
     const undoButton = document.createElement('button');
     undoButton.classList.add('yellow');
     undoButton.innerText = "Undo Read";
 
-    const actionContainer = document.createElement('div');
-    actionContainer.classList.add('action');
-    actionContainer.append(undoButton, removeButton);
-
     undoButton.addEventListener('click', function () {
+      /*
       bookObject.check = false;
       document.dispatchEvent(new Event(RENDER_EVENT));
+      */
+      undoBookFromCompleted(bookObject.id);
     });
-  } else if (!bookObject.check){
+
+    // =============Remove Button================
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('red');
+    removeButton.innerText = "Remove Book";
+
+    removeButton.addEventListener('click', function () {
+      removeTaskFromCompleted(bookObject.id);
+    });
+    
+    actionContainer.append(undoButton, removeButton);
+
+  } else{
   // =============Belum Selesai Dibaca================
     const doneButton = document.createElement('button');
     doneButton.classList.add('green');
     doneButton.innerText = "Done Read";
 
-    const actionContainer = document.createElement('div');
-    actionContainer.classList.add('action');
-    actionContainer.append(doneButton, removeButton);
-
     doneButton.addEventListener('click', function () {
-      bookObject.check = true;
-      document.dispatchEvent(new Event(RENDER_EVENT));
+      addBookToCompleted(bookObject.id);
     });
-  }
 
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('red');
+    removeButton.innerText = "Remove Book";
+
+    removeButton.addEventListener('click', function () {
+      removeTaskFromIncompleted(bookObject.id);
+    });
+    
+    actionContainer.append(doneButton, removeButton);
+  }
+  
 
   // Item Book Container 
   const itemBookContainer = document.createElement('article');
@@ -125,5 +171,7 @@ function makeBook(bookObject) {
 
   return itemBookContainer;
 }
+
+
 
 
