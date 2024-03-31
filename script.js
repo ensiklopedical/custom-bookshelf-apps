f129c403476e11805e9ce79413cdb0eadd759ef0
Faisal Ahmad Gifari <faisal.ahmadgifari21@mhs.uinjkt.ac.id>
2024-03-31 23:20:02 +0700

    FINAL: CLEAR

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ function searchBook() {
   const searchQuery = document.getElementById('searchTitle').value.toLowerCase();
   const searchResult = books.filter((book) => book.book.toLowerCase().includes(searchQuery));
   document.dispatchEvent(new CustomEvent(RENDER_EVENT, { detail: searchResult }));
-  console.log('searchBook: aman');
   AddRemoveSearch();
 }
 
@@ -30,7 +29,6 @@ function AddRemoveSearch() {
   removeSearchButton.classList.add('XXX');
   removeSearchButton.innerText = "X";
   submitSearch.append(removeSearchButton);
-  console.log('removeSearch: aman');
   removeSearchButton.addEventListener('click', function () {
     removeSearch();
     removeSearchButton.remove();
@@ -55,18 +53,15 @@ document.addEventListener('DOMContentLoaded', function () {
   submitSearch.addEventListener('submit', function (event) {
     event.preventDefault();
     searchBook();
-    console.log('Submit Search: aman');
   });
 });
 
 document.addEventListener(RENDER_EVENT, function (event) {
   const bookList = event.detail || books;
-  console.log(bookList);
   const incompleteBookList = document.getElementById('incompleteList');
   incompleteBookList.innerHTML = '';
   const completeBookList = document.getElementById('completeList');
   completeBookList.innerHTML = '';
-  console.log('RENDER EVENT: tengah');
   for (const bookItem of bookList) {
     if (bookItem.isComplete) {
       const bookElement = makeBook(bookItem);
@@ -76,7 +71,6 @@ document.addEventListener(RENDER_EVENT, function (event) {
       incompleteBookList.append(bookElement);
     }
   }
-  console.log('RENDER EVENT: aman');
 });
 
 function generateId() {
@@ -118,7 +112,6 @@ function makeBook(bookObject) {
   removeButton.classList.add('red');
   removeButton.innerText = "Remove Book";
   removeButton.setAttribute('id', 'removveButton');
-  console.log(removeButton);
   removeButton.addEventListener('click', function () {
     removeBook(bookObject.id);
   });
@@ -166,13 +159,8 @@ function undoBookFromCompleted(id) {
 
 function removeBook(id) {
   const confirmation = modelRemove();
-  console.log('removeBook: masuk diluar if');
-  console.log(confirmation);
-
   modelRemove().then(confirmation => {
-    console.log('removeBook: masuk diluar if');
     if (confirmation === true) {
-      console.log('removeBook: masuk true');
       const bookIndex = books.findIndex(bookItem => bookItem.id == id);
       books.splice(bookIndex, 1);
       document.dispatchEvent(new Event(RENDER_EVENT));
@@ -180,87 +168,26 @@ function removeBook(id) {
       removeSearchButton.remove();
       document.getElementById('searchTitle').value = '';
     } else {
-      console.log('removeBook: masuk false');
       return
     }
   });
 }
-  /*
-  if (confirmation == true) {
-    console.log('removeBook: masuk true');
-    const bookIndex = books.findIndex(bookItem => bookItem.id == id);
-    books.splice(bookIndex, 1);
-    document.dispatchEvent(new Event(RENDER_EVENT));
-    saveData();
-    removeSearchButton.remove();
-    document.getElementById('searchTitle').value = '';
-
-  } else if (confirmation == false) {
-    console.log('removeBook: masuk false');
-    return;
-
-  }
-  */
-
-  /*
-  const bookIndex = books.findIndex(bookItem => bookItem.id == id);
-  books.splice(bookIndex, 1);
-  document.dispatchEvent(new Event(RENDER_EVENT));
-  saveData();
-  removeSearchButton.remove();
-  document.getElementById('searchTitle').value = '';
-   */
-
-
 
 function modelRemove() {
-  console.log('modelRemove: masuk');
-  // return false;
   const modal = document.getElementById('remove-modal');
   modal.style.display = 'block';
-  console.log(modal);
   const span = document.getElementsByClassName('close')[0];
-  console.log(span);
-
-  /*
-  span.addEventListener('click', function () {
-    modal.style.display = 'none';
-    console.log('span on click: aman');
-  });
-  window.addEventListener('click', function (event) {
-    if (event.target == modal) {
-      modal.style.display = 'none';
-      console.log('window on click: aman');
-    }
-  });
-  */
-
   const yesButton = document.getElementById('yesRemove');
   const noButton = document.getElementById('noRemove');
 
   return new Promise((resolve, reject) => {
     yesButton.addEventListener('click', function () {
       modal.style.display = 'none';
-      console.log('yesButton on click: aman');
       resolve(true);
     });
     noButton.addEventListener('click', function () {
       modal.style.display = 'none';
-      console.log('noButton on click: aman');
       resolve(false);
     });
   });
-
-  /*
-  yesButton.addEventListener('click', function () {
-    modal.style.display = 'none';
-    console.log('yesButton on click: aman');
-    return true;
-  });
-  noButton.addEventListener('click', function () {
-    modal.style.display = 'none';
-    console.log('noButton on click: aman');
-    return false;
-  });
-  */
 }
